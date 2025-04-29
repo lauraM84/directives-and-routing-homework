8ee6500ea036ff858d4e7a390875a4993cd6d1ab
jerecede <jeremias10458@gmail.com>
2025-04-29 11:02:04 +0200

    directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SuperBtnDirective } from './directives/super-btn/super-btn.directive';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SuperBtnDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
